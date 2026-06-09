13018d79af5860e072b863c4076301b3203613c5
JackTench <79285604+JackTench@users.noreply.github.com>
2026-06-09 17:45:29 +0100

    Mark WASM support as an optional feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,21 @@
 use std::io::Cursor;
 
 use spex::parsing::XmlReader;
+
+#[cfg(feature = "wasm")]
 use wasm_bindgen::prelude::*;
 
 mod libresplit;
 mod livesplit;
 
-#[wasm_bindgen]
-pub fn convert(file: String) -> String {
+fn convert_inner(file: String) -> String {
     let cursor = Cursor::new(file);
     let xml = XmlReader::parse_auto(cursor).unwrap();
     let livesplit_data = livesplit::LiveSplitFile::new(xml);
     libresplit::LibreSplitFile::from_livesplit(livesplit_data).get()
+}
+
+#[cfg_attr(feature = "wasm", wasm_bindgen)]
+pub fn convert(file: String) -> String {
+    convert_inner(file)
 }
